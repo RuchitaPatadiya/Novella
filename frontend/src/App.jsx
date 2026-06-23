6c6eb6b5ee57267694811102d7ec0f0f3d21c535
RuchitaPatadiya <rpatadiya2004@gmail.com>
2026-06-24 01:23:02 +0530

    Initial Project Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='text-4xl font-bold color-blue-200'>
+          Novella
+    </div>
+  )
+}
+
+export default App
