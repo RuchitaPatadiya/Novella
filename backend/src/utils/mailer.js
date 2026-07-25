ef9a9a85793a2696108ef9ece8143568e7657e77
RuchitaPatadiya <rpatadiya2004@gmail.com>
2026-07-25 17:52:41 +0530

    fix

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,8 +1,12 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
 
-// Configure nodemailer transporter using Gmail SMTP
+// Configure nodemailer transporter using Gmail SMTP with forced IPv4 DNS lookup
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  lookup: (hostname, options, callback) => {
+    dns.lookup(hostname, { ...options, family: 4 }, callback);
+  },
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
